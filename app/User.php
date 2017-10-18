3a2e8238659cce0e0e8cedd5a58b092f3295a0e1
Kagawa Hoang <kagawa_hoang@Kagawa-no-MacBook-Pro.local>
2017-10-18 21:24:26 +0900

    add model,controller,view

--- app/User.php
+++ app/User.php
@@ -4,11 +4,21 @@ namespace App;
 
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Auth\Passwords\CanResetPassword;
+use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
+use Illuminate\Contracts\Auth\CanResetPassword as CanResetPasswordContract;
 
-class User extends Authenticatable
+
+class User extends Model implements AuthenticatableContract,CanResetPasswordContract
 {
-    use Notifiable;
-
+    use Notifiable,Authenticatable,CanResetPassword;
+     /**
+     * The database table used by the model.
+     *
+     * @var string
+     */
+    protected $table = 'users';	
     /**
      * The attributes that are mass assignable.
      *
@@ -17,7 +27,34 @@ class User extends Authenticatable
     protected $fillable = [
         'name', 'email', 'password',
     ];
-
+    // user has many posts
+  public function posts()
+  {
+    return $this->hasMany('App\Posts','author_id');
+  }
+  // user has many comments
+  public function comments()
+  {
+    return $this->hasMany('App\Comments','from_user');
+  }
+  public function can_post()
+  {
+    $role = $this->role;
+    if($role == 'author' || $role == 'admin')
+    {
+      return true;
+    }
+    return false;
+  }
+  public function is_admin()
+  {
+    $role = $this->role;
+    if($role == 'admin')
+    {
+      return true;
+    }
+    return false;
+  }
     /**
      * The attributes that should be hidden for arrays.
      *
