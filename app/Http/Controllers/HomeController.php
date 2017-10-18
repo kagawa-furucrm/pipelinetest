ddd23e8f625d347f3de7102284017f56f5dc7cf4
Kagawa Hoang <kagawa_hoang@Kagawa-no-MacBook-Pro.local>
2017-10-18 21:56:04 +0900

    modify homeController

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -23,6 +23,11 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+       //fetch 5 posts from database which are active and latest
+    $posts = Posts::where('active',1)->orderBy('created_at','desc')->paginate(5);
+    //page heading
+    $title = 'Latest Posts';
+    //return home.blade.php template from resources/views folder
+    return view('home')->withPosts($posts)->withTitle($title);
     }
 }
