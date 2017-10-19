12197b19cb62ee238b5e0e3b92fc6f6228b1707c
Kagawa Hoang <kagawa_hoang@Kagawa-no-MacBook-Pro.local>
2017-10-19 23:51:15 +0900

    change app view

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -36,10 +36,10 @@
           <ul class="nav navbar-nav navbar-right">
             @if (Auth::guest())
             <li>
-              <a href="{{ url('login') }}">Login</a>
+              <a href="{{ url('/auth/login') }}">Login</a>
             </li>
             <li>
-              <a href="{{ url('register') }}">Register</a>
+              <a href="{{ url('/auth/register') }}">Register</a>
             </li>
             @else
             <li class="dropdown">
