effe17573fd5b21b8f2101fa1b33124ab5bb1cb4
hoangkagawa <hoang.kagawa@mulodo.com>
2018-10-01 14:04:13 +0900

    update createuser table

--- database/migrations/2018_10_1_000000_create_users_table.php
+++ database/migrations/2018_10_1_000000_create_users_table.php
@@ -13,7 +13,6 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
- 	$this->down();       
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
